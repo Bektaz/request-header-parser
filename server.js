18246015881cd7772b732773f5e3731617927b2f
Bereket <berekettaz@gmail.com>
2016-01-12 13:57:37 +0500

    4th test commit

--- server.js
+++ server.js
@@ -5,9 +5,15 @@ var fs = require('fs');
 var port = process.env.PORT || 8080;
 
 var server = http.createServer(function(req, res){
-    res.writeHead(200,{'content-type':'text/plain'});
-    var ip = req.headers['x-forwarded-for'][0];
-    console.log(ip);
+    /*var ip = req.headers['x-forwarded-for'] || 
+    req.connection.remoteAddress || 
+    req.socket.remoteAddress ||
+    req.connection.socket.remoteAddress;
+    var soft = req.headers['user-agent'].split(' ').slice(1,6).join(' ');
+    soft = soft.slice(1,soft.length-1);
+    console.log(soft,'   ip: ',ip);*/
+    res.writeHead(200,{'content-type':'text/text'});
+    res.end(req.socket._peername.address);
 });
 server.listen(port, function(){
 	console.log('Our app is running on http://localhost:'+port);
