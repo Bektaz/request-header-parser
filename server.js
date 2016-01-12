d5be37d27575e9eebe12396b68f567af2cebb43b
Bereket <berekettaz@gmail.com>
2016-01-12 12:45:47 +0500

    3d test commit

--- server.js
+++ server.js
@@ -6,11 +6,7 @@ var port = process.env.PORT || 8080;
 
 var server = http.createServer(function(req, res){
     res.writeHead(200,{'content-type':'text/plain'});
-    var ip = req.headers['x-forwarded-for'] || 
-    req.connection.remoteAddress || 
-    req.socket.remoteAddress ||
-    req.connection.socket.remoteAddress;
-    res.end(ip);
+    var ip = req.headers['x-forwarded-for'][0];
     console.log(ip);
 });
 server.listen(port, function(){
